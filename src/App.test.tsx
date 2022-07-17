642a6b2d671cd70c334ce02bb313e1e87e767650
Luciene Clemente <luciene.clemente@unifei.edu.br>
2022-07-17 00:55:52 -0300

    Basic inputs tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Basic application view", () => {
+
+  it('should render an empity email input', () => {
+    render(<App />);
+
+    const emailElement = screen.getByRole("textbox", { name: /input-email/i });
+    expect(emailElement).toHaveValue("")
+  })
+
+  it('should render an empity password input', () => {
+    render(<App />);
+
+    const passwordElement = screen.getByRole("textbox", { name: /input-password/i });
+    expect(passwordElement).toHaveValue("")
+  })
+
+  it('should render an empity confirm password input', () => {
+    render(<App />);
+
+    const confirmPasswordElement = screen.getByRole("textbox", { name: /input-confirm-password/i });
+    expect(confirmPasswordElement).toHaveValue("")
+  })
+
+  it('should render a submit button', () => {
+    render(<App />);
+
+    const buttonElement = screen.getByRole("button", { name: /submit/i });
+    expect(buttonElement).toBeInTheDocument();
+  })
+})
